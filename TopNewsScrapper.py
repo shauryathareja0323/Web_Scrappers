b87da1eeda09673787bd19c953ab79bd35bc2a7f
Shaurya Thareja <72447531+shauryathareja0323@users.noreply.github.com>
2024-07-14 01:50:37 +0530

    Add files via upload

---
+++ TopNewsScrapper.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup as bs
+from selenium import webdriver
+
+driver=webdriver.Chrome()
+
+driver.get("https://www.hindustantimes.com/india-news")
+newsPage=driver.page_source
+newsPageHTML=bs(newsPage,'html.parser')
+
+newsBigBox=newsPageHTML.findAll("div", {"class":"cartHolder listView track timeAgo articleClick"})
+
+for box in newsBigBox:
+    heading=box.find_all('h3',{'class':'hdg3'})[0].text
+    desc=box.find_all('h2',{'class':'sortDec'})[0].text
+    print(heading)
+    print(desc)
+    print("\n\n\n")
+
+
